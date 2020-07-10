04944df2f90c6b550a168f9b60a2daf06e0fd4fa
Oscar Grainger <ogr@ehtrace.com>
2020-07-10 23:03:48 +0200

    feat: setup cases

---
+++ scripts.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const RESOLUTION = 50
+const TEAM_A_COLOR = 'blue'
+const TEAM_B_COLOR = 'white'
+
+
+const mobile = window.screenX < window.screenY
+let grid = []
+
+
+function randn_bm(min, max, skew) {
+    let u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) num = randn_bm(min, max, skew); // resample between 0 and 1 if out of range
+    num = Math.pow(num, skew); // Skew
+    num *= max - min; // Stretch to fill range
+    num += min; // offset to min
+    return num;
+}
+function setTeam(col) {
+    // TODO : init color as it should be
+    return randn_bm(0, RESOLUTION - 1, 1) < col ? TEAM_A_COLOR : TEAM_B_COLOR
+}
+function getRelative(row, col, x, y) {
+    let n = $(`${row + x}-${col + y}`)
+    console.log(n)
+}
+
+
+for (let row = 0; row < RESOLUTION; row++) {
+    let _row = []
+    for (let col = 0; col < RESOLUTION; col++) {
+        _row.push(setTeam(col))
+    }
+    grid.push(_row)
+}
+
+const app = $('#app')
+
+grid.forEach((row, i) => {
+    let $row = $(document.createElement("div"))
+    $row.addClass(`r r-${i}`)  
+    row.forEach((col, j) => {
+        let $col = $(document.createElement("div")).attr('id', `${i}-${j}`)
+        $col.addClass(`c c-${j} team-${col}`)
+        $col.css("height", `calc(100${mobile ? 'vw' : 'vh'} / ${RESOLUTION})`)
+        $col.css("width", `calc(100${mobile ? 'vw' : 'vh'} / ${RESOLUTION})`)
+        $col.css("background-color", col)      
+        $row.append($col)
+    })
+    app.append($row)
+})
+
+getRelative(5, 5, 0, 0)
+
+console.log(grid)
